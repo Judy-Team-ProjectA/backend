c1fe2c3221d61c9357169265879841f7087a50a8
Ellie <doris0707@naver.com>
2023-05-26 18:10:23 +0900

    feat: BusinessException setting

---
+++ src/main/java/Techeer/HealthIn/backend/global/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package Techeer.HealthIn.backend.global.exception;
+
+import Techeer.HealthIn.backend.global.response.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
