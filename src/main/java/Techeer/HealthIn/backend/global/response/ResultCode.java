b8fd4f90a64cef82cd54e59a997c1fbfffefd9cc
Ellie <doris0707@naver.com>
2023-06-03 23:03:28 +0900

    feat: result code 구체화

--- src/main/java/Techeer/HealthIn/backend/global/response/ResultCode.java
+++ src/main/java/Techeer/HealthIn/backend/global/response/ResultCode.java
@@ -7,9 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResultCode {
     // user
-    CREATE_USER_SUCCESS(200, "U001", "사용자 추가 성공"),
-    READ_USER_SUCCESS(200, "U002", "사용자 조회 성공"),
-    UPDATE_USER_SUCCESS(200, "U003", "사용자 정보 수정 성공")
+    USER_CREATE_SUCCESS(200, "U001", "사용자 추가 성공"),
+    USER_READ_ALL_SUCCESS(200, "U002", "모든 사용자 조회 성공"),
+    USER_READ_ONE_SUCCESS(200, "U003", "특정 사용자 조회 성공"),
+    USER_UPDATE_SUCCESS(200, "U004", "사용자 정보 수정 성공"),
+    USER_DELETE_SUCCESS(200, "U005", "사용자 정보 삭제 성공"),
+    USER_MATCHING_SUCCESS(200, "U006", "특정 유저와 매칭된 유저 정보 조회")
     ;
     private int status;
     private final String code;
